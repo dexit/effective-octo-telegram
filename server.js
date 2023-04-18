aa5dbdec375951db0321096ea7ada970b227ee71
dExIT <dexit@dyc.lv>
2023-04-18 20:32:31 +0100

    add new note logic re-write

--- server.js
+++ server.js
@@ -12,28 +12,32 @@ app.use(express.static('public'));  // serve static resources
 // Routes
 
 // API route for New entry, check for title if it is unique, if not unique, send error message
+// API route to create a new note
 app.post('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', (err, data) => {
-        if (err) {
-            res.sendStatus(500);
-        } else {
-            const notes = JSON.parse(data);
-            const title = req.body.title;
-            if (notes.some(note => note.title === title)) {
-                res.sendStatus(409);
-            } else {
-                notes.push(req.body);
-                fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-                    if (err) {
-                        res.sendStatus(500);
-                    } else {
-                        res.sendStatus(201);
-                    }
-                });
-            }
-            }
-    });
-});
+    // Read notes from db.json
+    const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+    
+    // Get note data from request body
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text
+    };
+    
+    // Check if title already exists
+    const titleExists = notes.some(note => note.title === newNote.title);
+  
+    if (titleExists) {
+      res.status(400).json({ error: 'Note with this title already exists' });
+    } else {
+      // Add new note to notes array
+      notes.push(newNote);
+    
+      // Write updated notes array to db.json
+      fs.writeFileSync('db.json', JSON.stringify(notes));
+    
+      res.json(newNote);
+    }
+  });
 // API route to delete a note by title
 app.delete('/api/notes/:title', (req, res) => {
     
